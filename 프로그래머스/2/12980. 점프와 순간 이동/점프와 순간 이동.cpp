6397109b2b56f46b1eba11be8e6feaf805c43dd7
InHyeok Jang <156999923+JangIn-40@users.noreply.github.com>
2024-09-13 10:05:37 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+
+    while (n)
+    {
+        if (n & 1)
+        {
+            ++ans;
+        }
+
+        n /= 2;
+    }
+
+    return ans;
+}
